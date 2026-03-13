207529d1a3b3788783285f646c8c77fe3d36efb9
Ushna Dar <ushna.darr@gmail.com>
2026-03-13 21:45:44 +0500

    System Overview

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,15 @@ function MainPage() {
     <div>
       <h1>System Overview</h1>
 
-      <div className="card-grid">
+      <div
+        className="card-grid"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gap: "30px",
+          marginTop: "30px"
+        }}
+      >
         <div className="card">
           <h3>CPU</h3>
           <p>Usage: --%</p>
