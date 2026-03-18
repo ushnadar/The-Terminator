b16dede9d6db0fdf3e1fb5f076fadb87cb26e36a
WyvRacersDev <142223454+WyvRacersDev@users.noreply.github.com>
2026-03-18 22:30:27 +0500

    added by network sorting to the process-info api
    does need the sort_by to be equal to "network" for per process network info

--- backend/info.py
+++ backend/info.py
@@ -128,8 +128,8 @@ class global_network_info(APIView,global_info):
         time.sleep(1)
         new = psutil.net_io_counters()
 
-        download_speed = (new.bytes_recv - old.bytes_recv) / 1024 / 1024
-        upload_speed = (new.bytes_sent - old.bytes_sent) / 1024 / 1024
+        download_speed = (new.bytes_recv - old.bytes_recv) / (1024**2)
+        upload_speed = (new.bytes_sent - old.bytes_sent) / (1024**2)
 
         return {
             # "connections": speeds_list,
@@ -177,52 +177,88 @@ class global_battery_info(APIView,global_info):
 
 class processes_info(APIView,global_info):
     def get_info(self, n=5, sort_by='cpu'):
-
-        procs = []
-
-        # First call to initialize
-        for p in psutil.process_iter(['pid', 'name', 'memory_info']): #this stupid library requires 2 calls to get the actual cpu usage (insane)
-            p.cpu_percent() 
-
-        import time
-        time.sleep(0.5) # 2 calls woh bhi separated DA HELL
-
-        cpu_count = psutil.cpu_count()
-
-        for p in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']):
-           
-            # if p.info['name'] == 'System Idle Process': #fake process that shows unused cpu usage for some messed up reason 
-            #     continue
-             
-            # if p.info['name'] == 'MsMpEng.exe': #windows anti malware executable (built in anti virus ab is se to pangay nahi le sakte lol)
-            #     continue
-
-            # if p.info['name'] == 'MemCompression': #memory compression ka program dat cant be killed
-            #     continue
-            
-            if p.info['name'] in processes_to_ignore:
-                continue
-
-            if p.info['pid'] == os.getpid(): #khud ko toh list mei nahi dalna na lol dont wanna flag ourselves as the resource hog we are lol RIGHT?
-                continue
+        
+        if (sort_by == 'network'):
+            procs = []
+            pid_io_before = {}
+            for p in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']):
                 
-            process_name=p.info['name']
-            process_cpu_usage=round(p.info['cpu_percent']/ cpu_count ,2) # lil bro returns by adding ALL THREAD's percentage so need to divide it by the count of threads
-            process_memory_usage= round(p.info['memory_info'].rss/(1024**2),2) #need mbs
+                if p.info['name'] in processes_to_ignore:
+                        continue
+                if p.info['pid'] == os.getpid():
+                        continue
+                pid_io_before[p.pid] = {
+                        'proc': p,
+                        'io':   p.io_counters()
+                    }
+               
 
-            procs.append({
-                'pid':   p.info['pid'], #is ke beghair browsers ke tabs mei confusion ho jana
-                'name':process_name,
-                'cpu':process_cpu_usage,
-                'mem': process_memory_usage,
-                # 'read':  read,
-                # 'write': write,
-            }
-            )
+            time.sleep(1)  # need 2 points to measure the speed
 
-        procs = sorted(procs, key=lambda x: x[sort_by], reverse=True)
+            for pid, data in pid_io_before.items():
+                
+                p   = data['proc']
+                io_after  = p.io_counters()
+                io_before = data['io']
 
-        return procs[:n]
+                upload_MBps   = round((io_after.write_bytes - io_before.write_bytes) /(1024**2), 2)
+                download_MBps = round((io_after.read_bytes  - io_before.read_bytes)  /(1024**2), 2)
+
+                procs.append({
+                    'pid':          p.info['pid'],
+                    'name':         p.info['name'],
+                    'upload_MBps':   upload_MBps,
+                    'download_MBps': download_MBps,
+                })
+                
+            procs = sorted(procs, key=lambda x: x['upload_MBps'] + x['download_MBps'], reverse=True)
+
+            return procs[:n]
+        else: 
+            procs = []
+
+            # First call to initialize
+            for p in psutil.process_iter(['pid', 'name', 'memory_info']): #this stupid library requires 2 calls to get the actual cpu usage (insane)
+                p.cpu_percent() 
+
+            time.sleep(0.5) # 2 calls woh bhi separated DA HELL
+
+            cpu_count = psutil.cpu_count()
+
+            for p in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']):
+            
+                # if p.info['name'] == 'System Idle Process': #fake process that shows unused cpu usage for some messed up reason 
+                #     continue
+                
+                # if p.info['name'] == 'MsMpEng.exe': #windows anti malware executable (built in anti virus ab is se to pangay nahi le sakte lol)
+                #     continue
+
+                # if p.info['name'] == 'MemCompression': #memory compression ka program dat cant be killed
+                #     continue
+                
+                if p.info['name'] in processes_to_ignore:
+                    continue
+
+                if p.info['pid'] == os.getpid(): #khud ko toh list mei nahi dalna na lol dont wanna flag ourselves as the resource hog we are lol RIGHT?
+                    continue
+                    
+                process_name=p.info['name']
+                process_cpu_usage=round(p.info['cpu_percent']/ cpu_count ,2) # lil bro returns by adding ALL THREAD's percentage so need to divide it by the count of threads
+                process_memory_usage= round(p.info['memory_info'].rss/(1024**2),2) #need mbs
+
+                procs.append({
+                    'pid':   p.info['pid'], #is ke beghair browsers ke tabs mei confusion ho jana
+                    'name':process_name,
+                    'cpu':process_cpu_usage,
+                    'mem': process_memory_usage,
+                    # 'read':  read,
+                    # 'write': write,
+                }
+                )
+
+            procs = sorted(procs, key=lambda x: x[sort_by], reverse=True)
+
+            return procs[:n]
     
     def get(self,request):
         n= int(request.query_params.get('n',5)) #size of kitne programs chahiyen list mei
