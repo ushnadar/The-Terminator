7620fb81e992ecc7280554b455a16f7f5a10db12
WyvRacersDev <142223454+WyvRacersDev@users.noreply.github.com>
2026-04-23 23:31:44 +0500

    alerts url

--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,7 @@ from .info import global_battery_info # ts be so redundant lmfao
 from .info import processes_info
 
 from . import SettingsAPI #settings wala api (totally separate)
-
+from . import AlertsAPI 
 urlpatterns = [
     path('api/cpu-info/', global_cpu_info.as_view()),
     path('api/memory-info/', global_memory_info.as_view()),
@@ -16,6 +16,11 @@ urlpatterns = [
     path('api/network-info/', global_network_info.as_view()),   
     path('api/battery-info/', global_battery_info.as_view()), 
     path('api/process-info/', processes_info.as_view()),
+    
     path('api/settings/', SettingsAPI.get_settings),
     path('api/settings/update/', SettingsAPI.update_settings),
+    
+    path('api/alerts/', AlertsAPI.get_alerts),
+    path('api/alerts/acknowledge/', AlertsAPI.acknowledge_alert),
+    path('api/alerts/delete/', AlertsAPI.delete_alert),
 ]
